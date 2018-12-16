2fb7d984943c80408c0aee9cdbd6f920613969cb
NanamiTakayama <ns.potable@gmail.com>
2018-12-16 17:46:21 +0900

    initial

---
+++ treer.py
@@ -0,0 +1,17 @@
+import datetime
+import time
+import subprocess
+from time import sleep
+
+cmd1 = "pwd"
+cmd2 = "tree"
+
+file = open('new.txt','a')
+file.write('new file\n')
+
+while True:
+    print(datetime.datetime.now())
+    subprocess.call(cmd1)
+    subprocess.call(cmd2)
+    #print(dt_now)
+    sleep(15)
